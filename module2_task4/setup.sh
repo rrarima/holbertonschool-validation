2481c27011b5ed8906279ae9d027ba014a7889f5
rrarima <5708@holbertonstudents.com>
2023-05-13 19:03:08 +1000

    chmod and removed files not needed

--- module2_task4/setup.sh
+++ module2_task4/setup.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-
-# Install required packages
-apt-get update && apt-get install -y curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt-get install -y ./hugo.deb
-rm hugo.deb
-apt-get install -y make
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+rm -f hugo_extended_0.109.0_linux-amd64.deb
 make build
