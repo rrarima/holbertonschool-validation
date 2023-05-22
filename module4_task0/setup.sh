0a6ece3d2bd2ea0e641c6de5e37392136611ee77
rrarima <5708@holbertonstudents.com>
2023-05-22 16:53:28 +1000

    removed sudo commands and added node installation

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y make git zip
+apt-get update && sudo apt-get install -y make git zip
 
-sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-sudo apt install ./hugo.deb
-sudo rm hugo.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
 
-sudo npm install -g markdownlint-cli
-sudo npm install -g markdown-link-check
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y --no-install-recommends nodejs=14.*
+npm install -g npm@7
 
-sudo apt-get remove golang-go
+npm install -g markdownlint-cli
+npm install -g markdown-link-check
+
+apt-get remove golang-go
 rm -rf /usr/local/go
 
 rm -rf dist/ 2> /dev/null
