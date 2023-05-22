0e48856e11d5d773c91e3d5b7e64c582c0196bee
rrarima <5708@holbertonstudents.com>
2023-05-22 15:58:55 +1000

    remove golang

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y make git wget zip
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
-dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+sudo apt-get update && sudo apt-get install -y make git zip
 
-sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
+sudo rm hugo.deb
+
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
+
+sudo apt-get remove golang-go
+rm -rf /usr/local/go
+
+rm -rf dist/ 2> /dev/null
