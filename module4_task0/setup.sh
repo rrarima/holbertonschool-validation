ea0490215cc035a5e28f6e67a30d45cf6d8fb0a1
rrarima <5708@holbertonstudents.com>
2023-05-23 14:27:06 +1000

    changed hugo install

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,9 +2,7 @@
 
 apt-get update && sudo apt-get install -y make git zip
 
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-apt install ./hugo.deb
-rm hugo.deb
+apt-get install -y hugo
 
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y --no-install-recommends nodejs=14.*
