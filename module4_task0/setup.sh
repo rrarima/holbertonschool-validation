7c47a725de18aa240900125729f19794332a5d33
rrarima <5708@holbertonstudents.com>
2023-05-23 17:33:49 +1000

    reverted changes to workflow

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y make git zip
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-apt-get install -y --no-install-recommends nodejs=14.*
-npm install -g npm@7
+sudo apt-get update && sudo apt-get install -y make git zip
 
-npm install -g markdownlint-cli
-npm install -g markdown-link-check
+# download/install minimal version to use template ananke
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
+#remove file after installation
+sudo rm hugo.deb
 
-# curl -L https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.deb -o golangci-lint.deb
-# apt install ./golangci-lint.deb
-# rm golangci-lint.deb
+# download/install linter
+
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
+
+# Uninstall go
+sudo apt-get remove golang-go
+rm -rf /usr/local/go
+
+# remove dist directory
+rm -rf dist/ 2> /dev/null
