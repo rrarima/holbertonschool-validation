53f15961c992b594d1285c71f7c77bf7d8dd219a
rrarima <5708@holbertonstudents.com>
2023-05-23 14:24:00 +1000

    changed to curl

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-apt-get update && apt-get install -y make git wget zip
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb
-dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
-rm hugo_extended_0.84.0_Linux-64bit.deb
+apt-get update && sudo apt-get install -y make git zip
 
-# sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
-apt-get update -y
-apt-get install nodejs npm -y
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
+
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y --no-install-recommends nodejs=14.*
+npm install -g npm@7
 
 npm install -g markdownlint-cli
 npm install -g markdown-link-check
+
+# curl -L https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.deb -o golangci-lint.deb
+# apt install ./golangci-lint.deb
+# rm golangci-lint.deb
