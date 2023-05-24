0ae0bd6090a30cfda508bd5b54c026f6ee1becb5
rrarima <5708@holbertonstudents.com>
2023-05-24 14:44:36 +1000

    set permissions sym link and workflow change

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
+apt-get update && apt-get install -y make wget
 
-apt-get update && sudo apt-get install -y make git zip
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.tar.gz
+tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
+mv hugo /usr/local/bin/
+rm hugo_extended_0.84.0_Linux-64bit.tar.gz
 
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-apt install ./hugo.deb
-rm hugo.deb
+curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
 
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-apt-get install -y --no-install-recommends nodejs=14.*
 npm install -g npm@7
 
-npm install -g markdownlint-cli
-npm install -g markdown-link-check
-
-# curl -L https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.deb -o golangci-lint.deb
-# apt install ./golangci-lint.deb
-# rm golangci-lint.deb
+npm install -g markdownlint-cli@v0.26.0
+npm install -g markdown-link-check@v3.8.6
