3ff3ec5c10259cd7950b36c23043a32253fd1fb9
rrarima <5708@holbertonstudents.com>
2023-05-24 14:04:23 +1000

    changed commands i setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y make git zip
+apt-get update && sudo apt-get install -y make git zip
 
-# download/install minimal version to use template ananke
-sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-sudo apt install ./hugo.deb
-#remove file after installation
-sudo rm hugo.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
 
-# download/install linter
+curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
 
-sudo npm install -g markdownlint-cli
-sudo npm install -g markdown-link-check
+npm install -g npm@7
 
-# Uninstall go
-sudo apt-get remove golang-go
-rm -rf /usr/local/go
-
-# remove dist directory
-rm -rf dist/ 2> /dev/null
+npm install -g markdownlint-cli@v0.26.0
+npm install -g markdown-link-check@v3.8.6
