21cbca13eb6ce3711f300215f2048c28ddc3e10c
rrarima <5708@holbertonstudents.com>
2023-05-24 14:36:57 +1000

    added workflow

---
+++ module4_task0/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update && sudo apt-get install -y make git zip
+
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
+
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y --no-install-recommends nodejs=14.*
+npm install -g npm@7
+
+npm install -g markdownlint-cli
+npm install -g markdown-link-check
+
+# curl -L https://github.com/golangci/golangci-lint/releases/download/v1.52.2/golangci-lint-1.52.2-linux-amd64.deb -o golangci-lint.deb
+# apt install ./golangci-lint.deb
+# rm golangci-lint.deb
